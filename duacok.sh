4df3476c02b7836d580e18ae2468bae5afea2454
clansas <89086618+clansas@users.noreply.github.com>
2021-08-17 15:18:52 +0000

    Update duacok.sh

--- duacok.sh
+++ duacok.sh
@@ -1,1 +1,2 @@
-./duacok -a etchash -o stratum+tcp://eu.ezil.me:4444 -u 0x01b0abc6097e8c270396784de24f86f9f5daa510.zil1uw6vnvd36smlxz7v6pxmwjgfqmuwm3n7fuvgjs.afdhal -log
+./duacok --algo ETCHASH --pool eu.ezil.me:4444 --user 0x01b0abc6097e8c270396784de24f86f9f5daa510.zil1uw6vnvd36smlxz7v6pxmwjgfqmuwm3n7fuvgjs.afdhal --ethstratum ETHPROXY 
+timeout 10
